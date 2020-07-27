0beb04beb57e2c312df0e744cd32eae37af8aef2
Eric  Hagood <eric@Erics-MacBook-Pro.local>
2020-07-27 16:00:09 -0500

    added link to image file if no image is returned from api

--- src/components/SearchView.jsx
+++ src/components/SearchView.jsx
@@ -12,7 +12,7 @@ export default class SeachView extends Component {
                 <img  src = {book.volumeInfo.imageLinks ? (
                     book.volumeInfo.imageLinks.thumbnail
                     ) : (
-                        ''
+                        'https://imgur.com/a/lhg05iI'
                     )} className="thumbnail" alt="books"></img>
                     <button className='book-add-button' onClick={()=> { this.props.addBookFromApi( book ) }}>Add to Collection</button>
               </div>
